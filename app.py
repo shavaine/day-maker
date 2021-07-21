c5a92bfdb0094e7ec14f95cd5b66a1ae96815524
shavaine <35738478+shavaine@users.noreply.github.com>
2021-07-20 20:21:31 -0400

    Hosting commit

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from os import environ
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'brownie101'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dayMaker.db'
+app.config['SECRET_KEY'] = environ.get('SUPER_SECRET') or 'brownie101'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///dayMaker.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# attach to DATABASE_URL for production
+# .replace("://", "ql://", 1)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
